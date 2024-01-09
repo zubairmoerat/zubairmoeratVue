ce588bec55fa8929ddfe0bec1f44be52d08562f2
zubairmoerat <zubairmoeratofficial@gmail.com>
2024-01-09 14:27:19 +0200

    storeDone

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,45 @@
 import { createStore } from 'vuex'
+const dataUrl = 'https://zubairmoerat.github.io/zubairmoeratData/data/'
 
 export default createStore({
   state: {
+    Home: null,
+    about: null,
+    education: null,
+    skills: null,
+    testimonials: null,
+    projects: null
   },
   getters: {
   },
   mutations: {
+    setHome(state, value) {
+      state.jobTitle = value
+    },
+    setAbout(state, value) {
+      state.about = value
+    },
+    setEducation(state, value) {
+      state.education = value
+    },
+    setSkills(state, value) {
+      state.skills = value
+    },
+    setTestimonials(state, value) {
+      state.testimonials = value
+    },
+    setProjects(state, value) {
+      state.projects = value
+    }
   },
   actions: {
-  },
-  modules: {
+    async fetchHome(context) {
+      let res = await fetch(dataUrl)
+      let { home } = await res.json()
+      context.commit('setHome', home)
+    },
+    modules: {
+    }
   }
 })
+
